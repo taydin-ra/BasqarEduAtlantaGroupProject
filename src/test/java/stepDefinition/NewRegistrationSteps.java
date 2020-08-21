1cc204383b3f35d411719f1ad89d12e88f9d7fd6
ersinoz <ersinozkan49@hotmail.com>
2020-08-21 17:01:16 -0400

    deneme

---
+++ src/test/java/stepDefinition/NewRegistrationSteps.java
@@ -0,0 +1,22 @@
+package stepDefinition;
+
+import cucumber.api.java.en.*;
+
+public class NewRegistrationSteps {
+
+    @Given("^Navigate to Entrance Exams$")
+    public void navigate_to_Entrance_Exams()  {
+
+    }
+
+    @When("^Create  a new registration approve$")
+    public void create_a_new_registration_approve() {
+
+    }
+
+    @Then("^New registration approve is successfully$")
+    public void new_registration_approve_is_successfully()  {
+
+    }
+
+}
