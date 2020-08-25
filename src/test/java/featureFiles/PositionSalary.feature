9c918ba67e9000d5bc7ce76be99a0c9dab20b260
zeynepholcal <olcalzeynephale@gmail.com>
2020-08-25 14:23:33 -0400

    Position Salary

---
+++ src/test/java/featureFiles/PositionSalary.feature
@@ -0,0 +1,12 @@
+Feature: Position salary functionality
+
+  Scenario: Login  with  username and  password
+    Given Navigate to Basqar: "https://test.basqar.techno.study/"
+    When Enter the username "daulet2030@gmail.com" and password "TechnoStudy123@"
+    And Click on loggin button
+    Then I am logged in successfully
+
+  Scenario: Create a new position salary
+    And Navigate to Position Salary page
+    When User create a position salary name as "New Position Salary"
+    Then Success message should be displayed
---
+++ src/test/java/stepDefinition/PositionSalarySteps.java
@@ -0,0 +1,33 @@
+package stepDefinition;
+
+import POMFiles.PositionSalaryPage;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class PositionSalarySteps {
+    PositionSalaryPage positionSalaryPage = new PositionSalaryPage();
+
+    @And("^Navigate to Position Salary page$")
+    public void navigate_to_Position_Salary_page() {
+
+        positionSalaryPage.findElementAndClickFunction("HumanResourcesButton");
+        positionSalaryPage.findElementAndClickFunction("Setup3button");
+        positionSalaryPage.findElementAndClickFunction("PositionSalary1Button");
+    }
+
+
+    @When("^User create a position salary name as \"([^\"]*)\"$")
+    public void user_create_a_position_salary_name_as(String arg1) {
+        positionSalaryPage.findElementAndClickFunction("AddButton");
+        positionSalaryPage.findElementAndSendKeys("NameInput",arg1);
+        positionSalaryPage.findElementAndClickFunction("SaveButton");
+    }
+
+
+    @Then("^Success message should be displayed$")
+    public void successMessageShouldBeDisplayed() {
+    }
+}
+
+
