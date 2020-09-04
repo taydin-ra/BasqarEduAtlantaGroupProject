40bbf1ec67badb3b71eceb7fae61d05353ebd827
Aghababa Mursaliyev <aghababa1986@gmail.com>
2020-09-04 11:32:30 -0700

    I created new Grade lavel 4

--- src/test/java/stepDefinition/GradeLavelFromSetupSteps.java
+++ src/test/java/stepDefinition/GradeLavelFromSetupSteps.java
@@ -4,10 +4,14 @@ import POMFiles.GradeLavelPage;
 import POMFiles.Parent;
 import Utilities.Driver;
 import cucumber.api.java.en.When;
+import junit.framework.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.Select;
+
+import java.util.Collections;
+import java.util.List;
 
 public class GradeLavelFromSetupSteps extends  Parent {
     GradeLavelPage grade=new GradeLavelPage ();
@@ -33,10 +37,12 @@ public class GradeLavelFromSetupSteps extends  Parent {
        // grade.active ();
         grade.translate ();
         grade.langButtonClick ();
-        grade.addRussian ();
-        grade.nameTraslateToRussian ();
-        grade.apply ();
+        grade.addEnglish ();
+        grade.nameTraslateToEnglish ();
+        grade.applyButtonClick ();
         grade.save ();
+        grade.succesGrade ();
+
 
 
 
